b7fdd0cc5475825a466a200000819de19408b8b7
Kasulitsky <Kasulitsky@mail.ru>
2023-02-02 12:33:03 +0300

    сделал первую программу

--- program1/Program.cs
+++ program1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = new Random().Next(-10,10);
+int numberB = new Random().Next(-10,10);
+int max = (0);
+int min = (0);
+if(numberA > numberB)
+{
+    max = numberA; min = numberB;
+}  
+else 
+{
+    max = numberB; min = numberA;
+}
+Console.Write("max"); Console.Write(max);  
+Console.Write("min"); Console.Write(min);  
