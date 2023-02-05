e0feaec9f42651da7136ec1bc42ed79957c403ce
Kasulitsky <Kasulitsky@mail.ru>
2023-02-05 14:41:55 +0300

    comleted Task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+//является ли этот день выходным.
+Console.WriteLine ("введите номер дня недели");
+int Mondey = 1;
+int Tuesday = 2;
+int Wednesday = 3;
+int Thursday = 4;
+int Friday = 5;
+int Saturday = 6;
+int Sunday  = 7;
+int a = Convert.ToInt32 (Console.ReadLine());
+if (a < 6)
+    {
+        Console.WriteLine ("no");
+    }
+else 
+    Console.WriteLine ("yes");
+
+
