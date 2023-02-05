9d7ba0fb1c33a11cc2b88d44dcc070c79c5257d7
Kasulitsky <Kasulitsky@mail.ru>
2023-02-05 13:13:33 +0300

    comleted  Task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+Console.WriteLine ("введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine ()); 
+int result = number / 10;
+Console.WriteLine ($"{number}, {result % 10}");
