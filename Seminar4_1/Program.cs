6285ae383efee434056ce344443d92682a8392b0
Kasulitsky <Kasulitsky@mail.ru>
2023-02-12 13:45:39 +0300

    сделал 1е задание 4го семинара

--- Seminar4_1/Program.cs
+++ Seminar4_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double Exponentiate(int A, int B)
+{
+    Console.WriteLine("введите первое число");
+    int f = Convert.ToInt32 (Console.ReadLine ());
+    Console.WriteLine("введите второе число");
+    int s = Convert.ToInt32 (Console.ReadLine ());
+    double result = Math.Pow (f, s);
+    return result; 
+}
+
+Console.WriteLine (Exponentiate (3, 2));
