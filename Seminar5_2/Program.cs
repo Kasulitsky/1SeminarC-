3c74d9ea08a24fdae76f7724a91963320a12a747
Kasulitsky <Kasulitsky@mail.ru>
2023-02-15 13:22:12 +0300

    сделал Smr5_2

---
+++ Seminar5_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.WriteLine("Hello, World!");
+int [] array = new int [9]; 
+int n = array.Length;
+int i = 0; 
+while (i < n)
+    {
+        array [i] = new Random (). Next (1, +100);
+        Console.WriteLine (array[i]);
+        i++;
+    }
+i = 0; 
+int sum = 0;
+while (i < n)
+    {
+        sum = sum + array [i];
+        i = i + 2;
+    }
+Console.WriteLine (sum);
