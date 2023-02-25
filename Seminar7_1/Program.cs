9a9d32b0502a5bacef13177be5cee672a5784698
Kasulitsky <Kasulitsky@mail.ru>
2023-02-25 18:15:43 +0300

    сделал 7_1

--- Seminar7_1/Program.cs
+++ Seminar7_1/Program.cs
@@ -1,7 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 double [,] CreateArray () 
 {
-    int a = Console.ReadLine();
-    int b = Console.ReadLine(); 
-    double [,] array = [a, b]
+    Console.WriteLine ("задайте размерноть двухмерного массива");
+    int a = Convert.ToInt32 (Console.ReadLine());
+    int b = Convert.ToInt32 (Console.ReadLine());
+    double [,] array = new double [a, b];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+         array [i,j] = new Random (). NextDouble () * 10;
+        Console.WriteLine(array [i,j]);   
+        }
+    }
+    return array;
 }
+CreateArray ();
