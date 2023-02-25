023e941da09903550c1f7dabf2a2f7dd718e99c3
Kasulitsky <Kasulitsky@mail.ru>
2023-02-25 17:07:43 +0300

    подготоил для седьмого семинара

---
+++ Seminar7_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+double [,] CreateArray () 
+{
+    int a = Console.ReadLine();
+    int b = Console.ReadLine(); 
+    double [,] array = [a, b]
+}
