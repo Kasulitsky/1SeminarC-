f243877d25bf55098c32595960ffae84c9d09a97
Kasulitsky <Kasulitsky@mail.ru>
2023-03-03 22:26:05 +0300

    сделал 7ой семинар, исправил_2

--- Seminar7_3/Program.cs
+++ Seminar7_3/Program.cs
@@ -17,37 +17,30 @@ double [,] CreateMassiv()
 }
 
 double [,] massiv = CreateMassiv();
+
 double sum1 = 0;
 double av1 = 0;
-for (int j = 0; j < massiv.GetLength(1);)
+for (int j = 0; j < massiv.GetLength(1); j++)
 {
     for (int i = 0; i < massiv.GetLength(0); i++)
         {
             sum1 = sum1 + massiv [i,j];
             av1 = sum1 / massiv.GetLength(0); 
         }
-    Console.WriteLine($"среднее арифметическое 1го столбца = {av1}");
-    break;    
+    Console.WriteLine($"среднее арифметическое = {av1}");
+     sum1 = 0;
+ av1 = 0;  
 }
-double sum2 = 0;
-double av2 = 0;
-for (int j = 1; j < massiv.GetLength(1);)
-{
-    for (int i = 0; i < massiv.GetLength(0); i++)
-        {
-            sum2 = sum2 + massiv [i,j];
-            av2 = sum2 / massiv.GetLength(0); 
-        }
-    Console.WriteLine($"среднее арифметическое 2го столбца = {av2}");
-    break;    
-}
+
+  
+
 
 // аналогично для следующих столбцов..  
 
 
 // хотел создать метод, но не понял как объявить аргументы, что бы можно было там подставлять, 
 
-// double [,] FindAverege (int a, int b)
+// double [,] FindAverege (int )
 // {
     
 // double [,] massiv = CreateMassiv();
